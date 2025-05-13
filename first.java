416b8eb93b187497d5f76b0893c8e2986346b172
mynk2003 <mayankbadhani5@gmail.com>
2025-05-13 11:58:40 +0530

    Add files via upload

--- first.java
+++ first.java
@@ -1,10 +1,11 @@
-public class First {
-    public static void main(String[] args) { 
-        for (int i = 4; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+public class first {
+    public static void main(String[] args){
+        System.out.println("NAME     MAIL-ID          EMPLOYEE-CODE    PHONE");
+        System.out.println("Parul    parul@cse        p03160          25764728");
+        System.out.println("Sonia    sonia@cse        p03161          25764728");
+        System.out.println("ankita   ankita@cse       p03162          25764729");
+        System.out.println("Gautam   gautam@cse       p03163          25764730");
+        System.out.println("Ram      Ram@cse          p03164          25764731");
+
     }
 }
--- second.java
+++ second.java
@@ -1,25 +1,5 @@
-public class Second {
-    public static void main(String[] args){
-        for (int i = 0; i < 4; i++) {
-            if (i == 0) {
-                for (int j = 1; j <= 4; j++) {
-                    System.out.print(j);
-                }
-            } 
-            else if (i == 1) {
-                for (int j = 4; j >= 2; j--) {
-                    System.out.print(j);
-                }
-            } 
-            else if (i == 2) {
-                for (int j = 3; j <= 4; j++) {
-                    System.out.print(j);
-                }
-            }
-            else if (i == 3) {
-                System.out.print(4);
-            }
-            System.out.println();
-        }
+public class second{
+    public static void main(String[] args) {
+        System.out.println("\"Can we print \"\\\" with System.out.println()?\"");
     }
 }
